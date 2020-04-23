d05bc2ac2e88fc794b61780ce22fae99a4d3ab61
Ty Jenkins <cientifik85@gmail.com>
2020-04-23 18:37:21 -0400

    added the shape of blocks in an array and the position for each rotation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector(".grid");
     const width = 10;
     const height = 20;
+    let currentPosition = 4;
 })
+
+//The Tetris Blocks
+const lBlock = [
+    [1,width+1,width*2+1,2],
+    [width,width+1,width+2,width*2+2],
+    [1,width+1,width*2+1,width*2],
+    [width,width*2,width*2+1,width*2+2]
+  ];
+
+  const zBlock = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ];
+
+  const tBlock = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ];
+
+  const oBlock = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ];
+
+  const iBlock = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ];
+
+  const theBlocks = [lBlock, zBlock, tBlock, oBlock, iBlock];
+
+  //Randomly pick a piece
+  let random = Math.floor(Math.random()*theBlocks.length);
+  let currentRotation = 0;
+  let current = theBlocks[random][currentRotation];
